44a221bbbb4b4b2b1facda10b64b6e1d31f7227b
systemis <cuthinh2002@icloud.com>
2017-07-17 17:56:54 +0700

    get link music with url :hammer:

--- server.js
+++ server.js
@@ -14,41 +14,29 @@ app.get('/', (req, res) => {
 var news = {};
 
 app.listen(3000, () => {
-    var c = new crawler({
-        maxConnections : 10,
-        // This will be called for each crawled page 
-        callback : function (error, res, done) {
-            if(error){
-                console.log(error);
-            }else{
-                var $ = res.$;
-                // $ is Cheerio by default 
-                //a lean implementation of core jQuery designed specifically for the server 
-                // console.log($("title").text());
-                // console.log($);
-                // console.log(res.$);
-            }
-            done();
-        }
-    });
-
     request("http://www.nhaccuatui.com/bai-hat/1-2-3-4-chi-dan.ivwlvn9YHjar.html", (err, res, body) => {
         if(err) return console.log("that bai");
+        var indexOfUrlData = body.indexOf("http://www.nhaccuatui.com/flash/xml?key1=");
+        var addBool = false;
+        var sondId  = "";
+        for(var i = indexOfUrlData; i < body.length; i++){
+            if(body[i] === '"'){
+                console.log(sondId);
+                i = body.length;
+                addBool = false;
+                request(`http://www.nhaccuatui.com/flash/xml?key1=${sondId}` , (err2, res2, body2) => {
+                    console.log(body2);
+                })
+            }
 
-        
+            if(addBool){
+                sondId += body[i];
+            }
+            
+            if(body[i] === '='){
+                addBool = true;
+            }
+        }
     })
 
-    request("http://www.nhaccuatui.com/flash/xml?key1=df0482589f13991652f12c7653d7014c" , (err, res, body) => {
-        console.log(body);
-    })
-    
-    // Queue just one URL, with default callback 
-    
-// Queue just one URL, with default callback 
-// Queue a list of URLs 
-// c.queue('http://www.amazon.com');
-// c.queue(['http://mp3.zing.vn/']);
-// c.queue('http://www.nhaccuatui.com/');
-c.queue('http://www.nhaccuatui.com/flash/xml?key1=270c28e286987bfdb2cfc59f88290231');
-    
 })
